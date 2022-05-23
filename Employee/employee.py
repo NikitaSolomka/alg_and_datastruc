5d711d5a10b906a11f0e26a484802a78911a4f0c
NikitaSolomka <m.s.solomka@student.khai.edu>
2022-05-23 16:29:20 +0200

    Practice 2

--- Employee/employee.py
+++ Employee/employee.py
@@ -1,18 +1,25 @@
 from datetime import datetime
+from datetime import date
+import names
+import random
 
 
 class Employee:
-    def __init__(self, name, surname, birthdate, gender, phone, position):
+    name: str
+    surname: str
+    birthdate: str
+    gender: str
+
+    def __init__(self, name: str, surname: str,  birthdate: str, gender: str):
         self.name = name
         self.surname = surname
         self.birthdate = birthdate
         self.gender = gender
-        self.phone = phone
-        self.position = position
 
-        self.check()
+    def __repr__(self):
+        return f'Person({self.name} {self.surname}, {self.gender}, {self.birthdate})'
 
-    def check(self):
+    def check_positions(self) -> list:
         age = int((datetime.today() - datetime.strptime(self.birthdate, "%Y-%m-%d")).days / 365.25)
         vacancies = list()
         if self.gender == "male":
@@ -25,20 +32,70 @@ class Employee:
                 vacancies.append("Уборщица")
             if age in range(20, 31):
                 vacancies.append("Секретарь")
+        return vacancies
 
-        self.get_info(vacancies)
-
-    def get_info(self, v):
+    def get_info(self):
+        v = self.check_positions()
         if len(v) >= 1:
-            print(f'{self.name} {self.surname}, Вам доступны данные вакансии: {", ".join(v)}. '
-                  f'Мы Вам перезвоним в течении 1-2 дней, по указанному Вами номеру ({self.phone})')
+            return f'{self.name} {self.surname}, Вам доступны данные вакансии: {", ".join(v)}'
         elif len(v) <= 0:
-            print(f'{self.name} {self.surname}, к сожалению для Вас доступных вакансий сейчас нет')
+            return f'{self.name} {self.surname}, к сожалению для Вас доступных вакансий сейчас нет'
+
+
+class Generator:
+    genders = ['female', 'male']
+
+    def create_date(self):
+        year = random.randint(1967, date.today().year)
+        month = random.randint(1, 12)
+        if month == 2:
+            day = random.randint(1, 28)
+        elif month in [1, 3, 5, 7, 8, 10, 12]:
+            day = random.randint(1, 31)
+        else:
+            day = random.randint(1, 30)
+        return f'{year}-{month}-{day}'
+
+    def generate_single(self) -> Employee:
+        gen = random.randint(0, 1)
+        gender = self.genders[gen]
+        name = names.get_first_name(gender)
+        surname = names.get_last_name()
+
+        birthdate = self.create_date()
+        return Employee(name, surname, birthdate, gender)
+
+    def generate_1000(self) -> list:
+        persons = list()
+        while True:
+            persons.append(self.generate_single())
+            if len(persons) == 1000:
+                break
+        return persons
+
+    def generate_10_000(self) -> list:
+        persons = list()
+        while True:
+            persons.append(self.generate_single())
+            if len(persons) == 10000:
+                break
+        return persons
 
 
 if __name__ == '__main__':
-    p1 = Employee("Lilly", "King", "2000-06-13", "female", "380631781345", "Секретарь")
-    p2 = Employee("Thomas", "Davies", "1996-06-13", "male", "38098973677", "Инженер")
-    p3 = Employee("Amelia", "Brown", "2003-06-13", "female", "38098434011", "Секретарь")
-    p4 = Employee("George", "Wilson", "1996-02-13", "male", "380992192687", "Курьер")
-    p5 = Employee("Connor", "Evans", "2003-06-13", "male", "3809824365337", "Инженер")
+    p = Employee("Lilly", "King", "2000-06-13", "female")
+    print(p)
+    print(p.get_info() + '\n')
+
+    p = Employee("Mark", "White", "1996-06-13", "male")
+    print(p)
+    print(p.get_info() + '\n')
+
+    g = Generator()
+    p = g.generate_single()
+    print(p)
+    print(p.get_info() + '\n')
+
+    print(f'{g.generate_1000()} \n')
+
+    # print(f'{g.generate_10_000()} \n')
